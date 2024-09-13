eede801a89528ea3e16cf45a17f347fba9ebe1a8
Christophe Laprudence <christophe.laprudence@howest.be>
2024-09-13 19:00:23 +0200

    fix: clean up

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,5 @@
+'use strict';
 import '../scss/styles.scss';
-
-if (import.meta.hot) {
-  import.meta.hot.accept(() => {
-    console.log('HMR');
-  });
-}
 
 const init = function () {
   console.log('hello world!');
