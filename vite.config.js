34c4f7012d45f6bb9dc2cd8b14e634da836c4a03
Christophe Laprudence <32491012+christophe-laprudence-howest@users.noreply.github.com>
2025-01-26 16:35:06 +0100

    fix: Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default ({ command, mode }) => {
   // Match ports in .ddev/config.yaml and config/vite.php
   const env = loadEnv(mode, process.cwd(), '');
   const HTTP_PORT = 3000;
-  const HTTPS_PORT = 3001;
+  const HTTPS_PORT = 3000;
 
   return {
     // In dev mode, we serve assets at the root of https://my.ddev.site:3000
@@ -32,6 +32,7 @@ export default ({ command, mode }) => {
       // Setting a specific origin ensures that your fonts & images load
       // correctly. Assumes you're accessing the front-end over https
       origin: env.PRIMARY_SITE_URL + ':' + HTTPS_PORT,
+      cors: true, // Allow cross-origin requests,
     },
   };
 };
